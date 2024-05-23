2936e42147772d9f2d68bf320754f3bf2033bbb7
Christianngono <christianngonoabanda384@gmail.com>
2024-05-23 08:18:00 +0200

    To add database.go

--- database.go
+++ database.go
@@ -19,6 +19,44 @@ func init() {
 
 func CreateTables() {
 	// Code pour créer les tables dans la base de données
+	createUsersTable := `CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT NOT NULL,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL
+	);`
+	createPostsTable := `CREATE TABLE IF NOT EXISTS posts (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        title TEXT NOT NULL,
+        content TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY (user_id) REFERENCES users(id)
+
+	);`
+	createCommentsTable := `CREATE TABLE IF NOT EXISTS comments (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		post_id INTEGER,
+		user_id INTEGER,
+		content TEXT,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY(post_id) REFERENCES posts(id),
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	);`
+	_, err := db.Exec(createUsersTable)
+	if err!= nil {
+        log.Fatalf("Error creating users table: %v\n", err)
+    }
+
+	_, err = db.Exec(createPostsTable)
+	if err!= nil {
+        log.Fatalf("Error creating posts table: %v\n", err)
+    }
+
+	_, err = db.Exec(createCommentsTable)
+	if err!= nil {
+        log.Fatalf("Error creating comments table: %v\n", err)
+    }
 }
 
 func Close() {
