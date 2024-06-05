edad56d6366fddd33eb914bdd0b8a352a7b8cbae
Christianngono <christianngonoabanda384@gmail.com>
2024-06-05 23:22:19 +0200

    To change it again

--- server/server.go
+++ server/server.go
@@ -4,7 +4,16 @@ import (
 	"forum"
 	"log"
 	"net/http"
+	"github.com/joho/godotenv"
+
 )
+
+func init() {
+	err := godotenv.Load()
+    if err!= nil {
+        log.Fatal("Error loading.env file")
+    }
+}
 
 func main() {
 	// Initialiser les tables de la base de données
