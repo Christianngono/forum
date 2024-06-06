655d07f6cc21382257a5d166a8104b4fe0eb1911
Christianngono <christianngonoabanda384@gmail.com>
2024-06-06 09:59:28 +0200

    To add it first time

---
+++ middleware.go
@@ -0,0 +1,16 @@
+package forum
+
+import (
+	"net/http"
+)
+func AuthenticateMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path!= "/login" {
+			if r.Header.Get("Authorization") == "" {
+                http.Error(w, "Unauthorized", http.StatusUnauthorized)
+                return
+            }
+		}
+        next.ServeHTTP(w, r)
+	})
+}
