be68fade19ffa17aa978911a883970b0f71a03d6
cahugo <hugo.cacioni@ynov.com>
2024-06-07 08:00:58 +0200

    eh vasi

---
+++ Javascript/Menuburger.js
@@ -0,0 +1,7 @@
+const menuBtn = document.getElementById('menu-btn');
+const navMenu = document.getElementById('nav-menu');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('open');
+    navMenu.classList.toggle('open');
+});
