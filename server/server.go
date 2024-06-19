814157af2a782442f4b066c69076d355393128b4
Christianngono <christianngonoabanda384@gmail.com>
2024-06-19 08:41:57 +0200

    To commit the file server

--- server/server.go
+++ server/server.go
@@ -24,20 +24,27 @@ func main() {
 	defer forum.DB.Close()
 
 	// Servir les fichiers statiques du répertoire 'static'
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../static"))))
 
 	http.HandleFunc("/logout", forum.LogoutHandler)
 	http.HandleFunc("/", forum.IndexHandler)
 	http.HandleFunc("/register", forum.RegisterHandler)
 	http.HandleFunc("/login", forum.LoginHandler)
 	http.HandleFunc("/create-post", forum.CreatePostHandler)
+	http.HandleFunc("/edit-post", forum.EditPostHandler)
+	http.HandleFunc("/delete-post", forum.DeletePostHandler)
+	http.HandleFunc("/get-dislike-post", forum.DislikePostHandler)
+	http.HandleFunc("/create-comment", forum.CreateCommentHandler)
+	http.HandleFunc("/edit-comment", forum.EditCommentHandler)
+	http.HandleFunc("/delete-comment", forum.DeleteCommentHandler)
+	http.HandleFunc("/get-like-post", forum.LikePostHandler)
+	http.HandleFunc("/get-dislike-post", forum.DislikePostHandler)
+	http.HandleFunc("/like-post", forum.LikePostHandler)
 	http.HandleFunc("/get-posts", forum.GetPostsHandler)
 	http.HandleFunc("/get-post", forum.GetPostHandler)
-    http.HandleFunc("/create-comment", forum.CreateCommentHandler)
     http.HandleFunc("/get-comments", forum.GetCommentsHandler)
 	http.HandleFunc("/get-comment", forum.GetCommentHandler)
-    http.HandleFunc("/like-post", forum.LikePostHandler)
-	http.HandleFunc("/get-dislike-post", forum.DislikePostHandler)
+	
 
 
 	log.Println("Server started on port 8080")
