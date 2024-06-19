911ea8777ee0e85af8179d450dca10f7caad75d1
Christianngono <christianngonoabanda384@gmail.com>
2024-06-19 08:46:18 +0200

    To review the file

--- handlers.go
+++ handlers.go
@@ -1,12 +1,10 @@
 package forum
 
 import (
-	"database/sql"
-	"encoding/json"
-	"html/template"
+	"text/template"
+	"fmt"
 	"net/http"
 	"os"
-	"log"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/sessions"
@@ -14,49 +12,37 @@ import (
 )
 
 type User struct {
-	ID       int    `json:"id"`
-	Email    string `json:"email"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Posts    []Post `json:"posts"`
-	Comments []Comment `json:"comments"`
+	ID       int       `json:"id"`
+	Email    string    `json:"email"`
+	Username string    `json:"username"`
+	Password string    `json:"-"`
 }
 
-var templates = template.Must(template.ParseFiles("../templates/index.html",
-	"../templates/register.html",
-	"../templates/login.html",
-	"../templates/create-post.html",
-	"../templates/posts.html",
-	"../templates/comments.html",
-	"../templates/create-comment.html",
-	"../templates/post.html",
-	"../templates/comment.html",
-	"../templates/likes.html",
-	"../templates/dislikes.html",
-))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-	err := templates.ExecuteTemplate(w, tmpl, data)
+	t, err := template.ParseFiles(fmt.Sprintf("/home/christian/forum/forum/templates/%s", tmpl))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	log.Println("Rendered template:", tmpl)
+	err = t.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+	} 
 }
 
 func getSessionStore() *sessions.CookieStore {
 	// Charger la clé secrète à partir des variables d'environnement
 	secretKey := os.Getenv("SESSION_SECRET_KEY")
 	if secretKey == "" {
-		// Si la clé n'est pas définie, retourner une erreur (ou utiliser une clé par défaut pour le développement)
-		// A remplacer par une vraie clé en production
-		println("Clé pas défini")
+		secretKey = "defaultSecretKey"
 	}
 	return sessions.NewCookieStore([]byte(secretKey))
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "index.html", nil)
+	http.ServeFile(w, r, "/home/christian/forum/forum/templates/index.html")
+	
 }
 
 func LogoutHandler(w http.ResponseWriter, r *http.Request) {
@@ -68,8 +54,8 @@ func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		renderTemplate(w, "register.html", nil)
+	if r.Method == http.MethodPost {
+		http.ServeFile(w, r, "/home/christian/forum/forum/templates/register.html")
 		return
 	}
 	var user User
@@ -80,13 +66,13 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Hacher le mot de passe avant de le stocker dans la base de données
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	if err != nil { 
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	user.Password = string(hashedPassword)
 
-	stmt, err := DB.Prepare("INSERT INTO users (email, username, password,) VALUES (?,?,?)")
+	stmt, err := DB.Prepare(`INSERT INTO users (email, username, password) VALUES (?, ?, ?)`)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -98,15 +84,13 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(map[string]string{"message": "User registered successfully"})
 	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	// Code pour gérer la connexion
-	if r.Method != http.MethodPost {
-		renderTemplate(w, "login.html", nil)
+	if r.Method == http.MethodPost {
+		http.ServeFile(w, r, "/home/christian/forum/forum/templates/login.html")
 		return
 	}
 
@@ -117,43 +101,26 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	var user User
 	err := DB.QueryRow("SELECT id, email, username, password FROM users WHERE email = ?", email).Scan(&user.ID, &user.Email, &user.Username, &user.Password)
 	if err != nil {
-		if err == sql.ErrNoRows {
-			http.Error(w, "Invalid email or password", http.StatusUnauthorized)
-		} else {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		return
+		http.Error(w, "Invalid email or password", http.StatusUnauthorized)
+		return	
 	}
 
 	// Comparer les mots de passe
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	if err != nil {
 		http.Error(w, "Invalid email or password", http.StatusUnauthorized)
-
+		return
 	}
 
 	// Créer une sessionID à l' UUID
-	sessionID := uuid.New().String()
+	sessionID, _ := uuid.NewRandom()
 
-	// Initialiser le store de session
 	store := getSessionStore()
-	session, err := store.Get(r, "session")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer session.Save(r, w)
+	session, _ := store.Get(r, "session")
 
-	// Stocker l'ID de session dans les valeurs de session
-	session.Values["post_id"] = user.ID
 	session.Values["user_id"] = user.ID
-	session.Values["username"] = user.Username
-	session.Values["email"] = user.Email
-	session.Values["session_id"] = sessionID
+	session.Values["session_id"] = sessionID.String()
 	session.Save(r, w)
-
-	/*w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(map[string]string{"message": "User logged in successfully"})*/
 
 	http.Redirect(w, r, "/get-posts", http.StatusSeeOther)
 }
